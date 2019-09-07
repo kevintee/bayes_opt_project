6a9789b2a6a6784ff24096e7ae008a7d2ea0ea18
Michael McCourt <mccourt@sigopt.com>
2019-09-06 19:19:20 -0700

    internet says I need this

--- read_test_cases.py
+++ read_test_cases.py
@@ -1,8 +1,9 @@
 import yaml
 
+
 def read_test_case(name='test_case_1'):
     with open('test_cases.yaml') as f:
-        data = yaml.load(f)
+        data = yaml.load(f, Loader=yaml.FullLoader)
     parameters = data[name]
 
     return parameters['deadhead_mean'], parameters['deadhead_variance']
